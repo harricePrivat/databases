c0e4463e2828f2ab85f1d8a19dfce13b353b0b6b
harricePrivat <briceprivat292@gmail.com>
2025-04-25 20:21:05 +0300

    rectify request test

--- back/index.js
+++ back/index.js
@@ -1,29 +1,16 @@
 const express= require('express')
 const {models} = require('./database')
-const { Matchs, Teams, Tournaments, Scores } = models
+const { matchs} = models
 
 // const auto= require('./sequelizequto')
 const app = express()
 
 app.use(express.json())
 
-app.get("/hello",(req,res)=>{
-    res.json({
-        message: "Mioty"
-    })
-})
-
 app.get("/tournois",async (req,res)=>{
     try {
-        const matchs = await Matchs.findAll({
-          include: [
-            { model: Teams, as: 'home_team' },
-            { model: Teams, as: 'away_team' },
-            { model: Tournaments },
-            { model: Scores }
-          ]
-        });
-        res.json(matchs);
+        const m = await matchs.findOne();
+        res.json(m);
       } catch (error) {
         console.error('Erreur lors de la récupération des matchs:', error);
         res.status(500).json({ message: 'Erreur serveur', error: error.message });
