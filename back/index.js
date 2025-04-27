fba3dfbe19e03fbe802c0ac91c7df4c34dd857ac
harricePrivat <briceprivat292@gmail.com>
2025-04-27 12:07:11 +0300

    show data

--- back/index.js
+++ back/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors())
 
 
-app.get("/tournois",async (req,res)=>{
+app.get("/nb-data",async (req,res)=>{
     try {
         const nbMatchs = await matchs.count();
         const nbTournois= await tournaments.count()
@@ -25,6 +25,49 @@ app.get("/tournois",async (req,res)=>{
         console.error('Erreur lors de la récupération des matchs:', error);
         res.status(500).json({ message: 'Erreur serveur', error: error.message });
       }
+})
+
+app.get("/teams",async (req,res)=>{
+    try{
+      const fiveTeams= await teams.findAll({
+        limit: 3
+      })
+      // console.log(fiveTeams[0].dataValues)
+      return res.status(200).json(fiveTeams)
+    }catch(error){
+      console.error("Erreur lors de la recuperation:", e)
+      res.status(500).json({ message: 'Erreur serveur', error: error.message });    
+    }
+
+})
+
+app.get("/five-matchs",async (req,res)=>{
+  try{
+    const fiveMatchs= await matchs.findAll({
+      limit: 5,
+      include: [
+       {
+        model: teams
+       } 
+      ]
+    })
+    return res.status(200).json(fiveMatchs)
+  }catch(e){
+    console.error("Erreur lors de la recuperation:", e)
+    res.status(500).json({ message: 'Erreur serveur', error: error.message });    
+  }
+})
+
+app.get("/five-tournaments",async (req,res)=>{
+  try{
+      const ft= await tournaments.findAll({
+        limit: 5
+      })
+      res.status(200).json(ft)
+  }catch(e){
+    console.error("Erreur lors de la recuperation:", e)
+    res.status(500).json({ message: 'Erreur serveur', error: error.message });    
+  }
 })
 
 app.listen(3000,()=>{
